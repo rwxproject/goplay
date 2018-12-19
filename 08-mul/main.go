e98aa8f1ac3ae760c515364ec1ab96ee11c0d0f7
Fabiano Fernandes <rwxproject@gmail.com>
2018-12-18 20:22:05 -0500

    update

---
+++ 08-mul/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	fmt.Println("CPUs: ", runtime.NumCPU())
+	fmt.Println("Go Routines:", runtime.NumGoroutine())
+
+	counter := 0
+
+	const gs = 100
+
+	var wg sync.WaitGroup
+	wg.Add(gs)
+
+	var mu sync.Mutex
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			mu.Lock()
+			v := counter
+			// runtime.Gosched()
+			v++
+			counter = v
+			mu.Unlock()
+			wg.Done()
+		}()
+		fmt.Println("Go Routines::", runtime.NumGoroutine())
+	}
+	wg.Wait()
+	fmt.Println("Go Routines:::", runtime.NumGoroutine())
+	fmt.Println("count: ", counter)
+}
