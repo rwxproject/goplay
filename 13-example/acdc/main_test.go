5f99fb4850976e77abfc448b720d7b4f55ff0b3a
rwxproject <rwxproject@gmail.com>
2018-12-19 21:35:32 -0500

    update

--- 13-example/acdc/main_test.go
+++ 13-example/acdc/main_test.go
@@ -1,9 +1,25 @@
 package acdc
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+	s := Sum(1, 2, 3)
+	if s != 6 {
+		t.Error("got", s, "expected", 6)
+	}
+}
 
 func ExampleSum() {
 	fmt.Println(Sum(1, 2, 3))
 	// Output:
 	// 6
+}
+
+func BenchmarkSum(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Sum(1, 2, 3)
+	}
 }
--- 13-example/main.go
+++ 13-example/main.go
@@ -9,3 +9,9 @@ import (
 func main() {
 	fmt.Println(acdc.Sum(1, 2, 3, 4, 5))
 }
+
+// to run benchmark run:
+// go test -bench .
+
+// to test run
+// go test
